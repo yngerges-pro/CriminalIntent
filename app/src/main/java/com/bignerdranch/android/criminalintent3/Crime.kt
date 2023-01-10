da2a249780535225a6fb33c7ca42101000a9081d
yngerges-pro <yghmny@gmail.com>
2023-01-09 17:44:45 -0800

    Finished adding the icons

--- app/src/main/java/com/bignerdranch/android/criminalintent3/Crime.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent3/Crime.kt
@@ -1,7 +1,10 @@
 package com.bignerdranch.android.criminalintent3
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 import java.util.Date
+
 
 data class Crime(val id: UUID = UUID.randomUUID(),
                  var title: String = "",
